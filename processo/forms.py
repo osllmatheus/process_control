53855e7b7b16617e5db42f93cd01b6dff43fbb42
Matheus Oliveira <osllmatheus@gmail.com>
2024-11-06 12:33:31 -0300

    forms processo

--- processo/forms.py
+++ processo/forms.py
@@ -8,14 +8,8 @@ class ProcessoForm(forms.ModelForm):
         model = Processo
         fields = ('nome_processo','ferramenta_utilizada',
                   'responsavel','documentacao','area')
-        widgets = {
-            'subprocesso' : forms.CheckboxSelectMultiple,
-        }
         
 class SubProcessoForm(forms.ModelForm):
     class Meta:
         model = SubProcesso
-        fields = ('nome_subprocesso','parent_subprocesso')
-        widgets = {
-            'parent_subprocesso' : forms.CheckboxSelectMultiple,
-        }
+        fields = ('nome_subprocesso','processo','parent_subprocesso')
