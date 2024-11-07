240d6a9724565adb96ab5ae9c0aee4ffd1942a4f
Matheus Oliveira <osllmatheus@gmail.com>
2024-11-07 17:34:18 -0300

    MPTT

--- processo/forms.py
+++ processo/forms.py
@@ -12,4 +12,15 @@ class ProcessoForm(forms.ModelForm):
 class SubProcessoForm(forms.ModelForm):
     class Meta:
         model = SubProcesso
-        fields = ('nome_subprocesso','status','processo','parent_subprocesso')
+        fields = ('nome_subprocesso','status','processo','parent')
+    
+    parent = forms.ModelChoiceField(
+        queryset=SubProcesso.objects.all(),
+        required=False,
+        label="Subprocesso Parent",
+        empty_label="-----"
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['parent'].queryset = SubProcesso.objects.all().order_by('lft')  # Ordena os subprocessos por ordem de árvore
